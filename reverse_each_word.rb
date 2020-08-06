41b99dff6dd460a152103a6067a6f024b719cc87
Devin Davis <devinrdavis555@gmail.com>
2020-08-06 19:22:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
   reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  reversed_array.join(" ")
 end
